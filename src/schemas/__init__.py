34ed9e73e1d0736e566497bd7e1c7cd575cf7f23
Andriy Shafran <shafran.andriy@gmail.com>
2026-05-28 07:25:11 +0300

    feat: Export profile schemas from schemas package

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -17,3 +17,7 @@ from schemas.accounts import (
     TokenRefreshRequestSchema,
     TokenRefreshResponseSchema
 )
+from schemas.profiles import (
+    ProfileCreateSchema,
+    ProfileResponseSchema
+)
